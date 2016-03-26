4d3f9c451df76ef9a0751d7367ac825e87f0ffe0
Snehal Shendware <snehalshendware1@gmail.com>
2016-03-25 17:52:23 -0700

    Utility based on commands from clients

--- Extra Credit Lab/simplesockserver.js
+++ Extra Credit Lab/simplesockserver.js
@@ -1,13 +1,30 @@
 // simple socket server
 var net = require('net');
-var sum = 0;
+var total = 0;
 net.createServer(function (sock) {
     console.log("Incoming connection accepted");
     sock.on('data', function (data) {
       console.log("Received from client: " +data);
       var d = JSON.parse(data);
-        sum += Number(d.val);
-        sock.write(sum.toString(), function() {
+
+      switch (d.command) {
+        case 'a':
+                total += Number(d.val);
+          break;
+        case 'm':
+                total -= Number(d.val);
+          break;
+        case 's':
+                total = Number(d.val);
+          break;
+        case 'q':
+                process.exit(0);
+          break;
+        default:
+                total = "Invalid request specification"
+
+      }
+        sock.write(total.toString(), function() {
         	console.log("Finished response to client");
         })
     }).on('error', function (e) {
