8a058d714ba97bb3ff0963da13d8dc29bd6b0228
Snehal Shendware <snehalshendware1@gmail.com>
2016-04-08 19:39:19 -0700

    Express Module with EJS [Lab4 Task2] Intial Commit

---
+++ Lab4/task2.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var ejs = require('ejs');
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.set('view engine','ejs');
+app.set('views', '.');
+
+var allRecords = [];
+
+app.get('/', function (req, res) {
+  res.locals.status = null;
+  res.locals.count = null;
+  res.render('index2');
+});
+
+app.post('/post_coder', function (req, res) {
+  storeRecord(req, res);
+});
+
+app.get('/coders', function (req, res) {
+  res.render('displayRecords',{records:allRecords});
+});
+
+app.listen(8081);
+
+function storeRecord(req, res) {
+
+  var status = ['SUCCESS','FAILURE']
+
+  if(!req.body['fname'] == '' && !req.body['lname'] == '')
+	   allRecords.push(req.body);
+
+  res.render('index2', {status:status[0],count:allRecords.length+' USER ADDED'});
+
+  res.end();
+}
